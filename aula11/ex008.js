1b6bee9b8f6ea0c4e36505129ece897161ed2af1
Natan Cruz Oliveira <136939180+NatanCruzOliveira@users.noreply.github.com>
2023-10-05 20:07:05 -0300

    criei meu desafio 12

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade do carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade.MULTADO!`)
+}
+console.log(`Dirija sempre utilizando cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+console.log(`Vivendo no ${pais}`)
+if (pais == 'brasil') {
+    console.log('você é brasileiro') 
+} else {
+    console.log('você é estrangeiro')
+}
