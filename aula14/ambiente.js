b3c475facd8ce9b8516001b658c16309fd4b9e3c
Natan Cruz Oliveira <136939180+NatanCruzOliveira@users.noreply.github.com>
2024-02-08 00:23:19 -0300

    Create exercicio contador com passo

---
+++ aula14/ambiente.js
@@ -0,0 +1,25 @@
+/*
+
+var c = 1
+
+while (c <= 5) {
+    console.log(`A variavel C agora vale ${c}`)
+    c++
+}
+
+*/
+
+/*
+
+var c = 1
+
+do {
+    console.log(`A variavel C agora vale ${c}`)
+    c++
+} while (c <= 5)
+
+*/
+
+for (var c = 1;c < 10;c++) {
+    console.log(`A variavel C agora vale ${c}`)
+}
