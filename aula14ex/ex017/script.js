d36dd8f9e3d435b11a3dcf1335d3e768509c6af6
Natan Cruz Oliveira <136939180+NatanCruzOliveira@users.noreply.github.com>
2024-02-09 03:33:56 -0300

    create tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,24 @@
+function calcular() {
+    var num = window.document.getElementById('txtn')
+    var tab = window.document.getElementById('seltab')
 
+    if (num.value.length != 0) {
+        var n = Number(num.value)
+        tab.innerHTML = ''
+
+        for (c=1; c <= 10; c += 1) {
+
+            var item = document.createElement('option')
+            tab.appendChild(item)
+
+            item.value = `tab${c}`
+            item.text = `${n} x ${c} = ${n*c}`
+            
+        }
+
+    } else {
+        alert('Por favor digite um número!')
+    }
+
+    
+}
