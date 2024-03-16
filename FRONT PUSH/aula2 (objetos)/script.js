fd2165d1f4358b6bb6949b95b0bf171db884d221
Natan Cruz Oliveira <136939180+NatanCruzOliveira@users.noreply.github.com>
2024-03-16 01:14:37 -0300

    update script.js

--- FRONT PUSH/aula2 (objetos)/script.js
+++ FRONT PUSH/aula2 (objetos)/script.js
@@ -1,19 +1,54 @@
-/*function olamundo() {
-    console.log('olá mundo!')
+//OBJETO
+
+//Conjunto de variáveis (atributos) e funções (métodos).
+/*
+
+var quadrado = {
+    lado: 4,
+    area: function(lado) {
+        return lado * lado;
+    },
+    perimetro: function(lado) {
+        return4 * lado;
+    }
 }
 
-olamundo();
+console.log(quadrado.lado());
+*/
 
-function arearetangulo(a, b) {
-    return a * b;
+/*
+var quadrado = {
+    lado: 4,
+
+    area: function() {
+        return this.lado * this.lado;
+    },
+
+    perimetro: function() {
+        return this.lado * 4;
+    }
 }
 
-console.log(arearetangulo(4, 2));
+console.log(quadrado.area());
 
 */
 
-function uppername(name) {
-    return name.toUpperCase();
+var retangulo = {
+    a: 4,
+    b: 10,
+
+    area: function() {
+        return this.a * this.b;
+    },
+
+    perimetro: function() {
+        return ( 2 * this.a + 2 * this.b);
+    }
 }
 
-console.log(uppername('jessica'));
+retangulo.a = 5;
+retangulo.b = 8;
+
+retangulo.border = 1;
+
+console.log(retangulo.perimetro());
