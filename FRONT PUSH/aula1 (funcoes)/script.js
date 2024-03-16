94c6763c0a50ef97077d25ca8de84aa5bfd839a9
Natan Cruz Oliveira <136939180+NatanCruzOliveira@users.noreply.github.com>
2024-03-15 22:48:54 -0300

    atualização

---
+++ FRONT PUSH/aula1 (funcoes)/script.js
@@ -0,0 +1,19 @@
+/*function olamundo() {
+    console.log('olá mundo!')
+}
+
+olamundo();
+
+function arearetangulo(a, b) {
+    return a * b;
+}
+
+console.log(arearetangulo(4, 2));
+
+*/
+
+function uppername(name) {
+    return name.toUpperCase();
+}
+
+console.log(uppername('jessica'));
