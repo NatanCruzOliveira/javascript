95907168e4571851ab15b5153c487d02c9dbc845
Natan Cruz Oliveira <136939180+NatanCruzOliveira@users.noreply.github.com>
2024-03-18 22:54:43 -0300

    efwfwer

---
+++ FRONT PUSH/aula4 (DOM)/script.js
@@ -0,0 +1,27 @@
+
+//DOM (Document Object Model)
+
+//É uma interface que representa o HTML por meio de objetos.
+
+//Com ela é possível manipular a estrutura, conteúdo, estilo.
+
+window
+    location
+    document
+        html
+            head
+                Meta
+                title
+            body
+                h1
+                p
+                span
+                ul
+                button
+                form
+
+const h4 = document.querySelector('h4');
+
+console.log(h4.innerText)
+
+console.log(h4)
