308ab87d2e871466c6cc2f2e0e75f8512f593fdd
Natan Cruz Oliveira <136939180+NatanCruzOliveira@users.noreply.github.com>
2024-03-19 00:41:16 -0300

    ygugig]

--- FRONT PUSH/aula4 (DOM)/script.js
+++ FRONT PUSH/aula4 (DOM)/script.js
@@ -5,6 +5,7 @@
 
 //Com ela é possível manipular a estrutura, conteúdo, estilo.
 
+/*
 window
     location
     document
@@ -19,9 +20,48 @@ window
                 ul
                 button
                 form
+*/
 
-const h4 = document.querySelector('h4');
+/*
+1 - SELETORES DE ELEMENTOS
 
-console.log(h4.innerText)
+var titulo = document.getElementById('titulo')
+console.log(titulo)
 
-console.log(h4)
+var subtitle = document.getElementsByClassName('subtitle')
+console.log(subtitle[0])
+*/
+
+/*
+ Selecionando e adiconado classe no html pelo JS
+
+const items = document.querySelectorAll('.menu .menu-item')
+console.log(items)
+
+items.forEach(function(item){
+    item.classList.add('item-classe')
+})
+
+*/
+
+/*
+
+// 2 - CLASSES E ATRIBUTOS (ADICIONAR NO HTML PELO JS)
+const img = document.querySelector('img');
+const menu = document.querySelector('.menu')
+
+console.log(menu.className); //string com as classes da tag
+console.log(menu.classList); //Lista um 'ARRAY' com as classes
+
+menu.classList.add('teste', 'lista'); //Adiciona novas classes
+menu.classList.remove('lista'); //Remove a Classe
+menu.classList.toggle('active'); // Verifica se tem um a classe, caso tenha remove, caso não tenha ele adicona
+console.log(menu.classList.contains('active')); //Verifica se já existe essa classe e retorna true ou false (boolean)
+menu.classList.replace('teste', 'test'); //Troca uma classe por outra
+
+console.log(img.getAttribute('alt')); //puxa o que tem escrito no atributo
+console.log(img.setAttribute('alt', 'Aqui é outra msg')); //Muda o que estava no atributo ou cria um novo
+
+img.removeAttribute('src'); //Remove um atributo
+
+*/
