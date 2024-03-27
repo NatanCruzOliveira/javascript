b92530ef333faee03e2a8b511764f80ce464d504
Natan Cruz Oliveira <136939180+NatanCruzOliveira@users.noreply.github.com>
2024-03-27 00:29:53 -0300

    axios

--- FRONT PUSH/aula11 (axios)/script.js
+++ FRONT PUSH/aula11 (axios)/script.js
@@ -1,45 +1,8 @@
-var mypromise = function() {
-    return new Promise(function(resolve, reject) {
-    
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.github.com/users/natancruzoliveira')
-    xhr.send(null);
-    
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState === 4) {
-            if(xhr.status === 200) {
-                resolve(JSON.parse(xhr.responseText));
-            }else {
-                reject('erro');
-            }
-        }
-    } //Vê se está funcionando a requisição
-
-    })
-} 
-
-mypromise()
+axios.get('https://api.github.com/users/natancruzoliveira')
 .then(function(response) {
-    console.log(response)
+    console.log(response);
 })
 
 .catch(function(error) {
-    console.log(error)
+    console.log(error);
 })
-
-
-
-
-
-
-
-let xhr = new XMLHttpRequest(); //instanciando uma classe
-
-xhr.open('GET', 'https://api.github.com/users/natancruzoliveira')
-xhr.send(null);
-
-xhr.onreadystatechange = function() {
-    if(xhr.readyState === 4) {
-        console.log(JSON.parse(xhr.responseText))
-    }
-} //Vê se está funcionando a requisição
