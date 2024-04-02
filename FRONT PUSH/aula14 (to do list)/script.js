1d90e6c8f664e4131fe5ea3b87044967f483f206
Natan Cruz Oliveira <136939180+NatanCruzOliveira@users.noreply.github.com>
2024-04-02 00:01:55 -0300

    ws

--- FRONT PUSH/aula14 (to do list)/script.js
+++ FRONT PUSH/aula14 (to do list)/script.js
@@ -1,16 +1,51 @@
-document.documentElement.addEventListener('mousemove', mousemove)
 
-function mousemove(event) {
+var listelement = document.querySelector('.list');
+var textelement = document.querySelector('input');
+var buttonelement = Document.querySelector('button');
 
-    const {clientX, clientY} = event;
+var arr = [
+    'ver aulas do frontpush de JS',
+    'Tentar executar sozinho',
+    'Fazer um Café',
+];
+console.log(arr.length)
 
-    //const clientX = event.clientX;
-   // const clientY = event.clientY;
-    console.log(clientX, clientY);
+function renderTodo() {
+    /*
+    for(var i = 0; i < arr.length; i++) {
+        console.log(i)
+    }
+    */
+    arr.forEach(function(item, index) {
+        var lielement = document.createElement('li');
+        var litext = document.createTextNode(item);
+
+        var linkLi = document.createElement('a');
+        var TextLink = document.createTextNode('Excluir');
+
+        linkLi.appendChild(TextLink)
+        linkLi.setAttribute('href', '#')
+        linkLi.setAttribute('onclick', 'deleteitem('+ index +')')
+
+        lielement.appendChild(litext);
+        lielement.appendChild(linkLi);
+        listelement.appendChild(lielement);
+    }) 
+
+    }
+
+renderTodo();
+
+buttonelement.addEventListener('click', additem)
+
+function additem() {
+    var itemtexto = textelement.value;
+    arr.push(itemtexto);
+    renderTodo();
 }
 
-const carros = ['volks', 'fiat', 'bmw'];
+function deleteitem(pos) {
+    arr.splice(pos, 1);
 
-const [marca1, marca2, marca3] = carros;
-
-console.log(marca1)
+    renderTodo();
+}
