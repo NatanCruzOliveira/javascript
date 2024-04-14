88451862b7d1f891de7ed7cf2064c939cb2e8bcd
Natan Cruz Oliveira <136939180+NatanCruzOliveira@users.noreply.github.com>
2024-04-14 03:57:20 -0300

    fcttryu

---
+++ FRONT_PUSH/REACT.JS/blog/src/components/Header/index.js
@@ -0,0 +1,18 @@
+
+function Header() {
+    return (
+        <header>
+            <div className='logo'>
+
+            </div>
+            <div className='search'>
+
+            </div>
+            <div className='menu'>
+                
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ FRONT_PUSH/REACT.JS/blog/src/components/Post/index.js
@@ -0,0 +1,9 @@
+
+
+function Post() {
+    return(
+        <h1>Post.js</h1>
+    );
+}
+
+export default Post;
