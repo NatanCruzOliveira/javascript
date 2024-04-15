60cf7746dfab627e3be8de6a436b63c29893f272
Natan Cruz Oliveira <136939180+NatanCruzOliveira@users.noreply.github.com>
2024-04-15 00:05:50 -0300

    create header

--- FRONT_PUSH/REACT.JS/blog/src/components/Header/index.js
+++ FRONT_PUSH/REACT.JS/blog/src/components/Header/index.js
@@ -6,16 +6,18 @@ function Header() {
     const decrLogo = 'Essa é a logo do Blog';
 
     return (
-        <header>
+        <header className='flex-space-between'>
             <div className='logo'>
                 <img src = {logo} width= '100px' alt = {decrLogo}/>
             </div>
             <div className='search'>
-
+                <input type='text' name='search' className='input-search' placeholder='Pesquise por notícias, artigos e muito mais'></input>
             </div>
-            <div className='menu'>
-                
-            </div>
+            <ul className='menu'>
+                <li> <a href='#' className='nav-link'> Categorias </a> </li>
+                <li> <a href='#' className='nav-link'> Sobre </a> </li>
+                <li> <a href='#' className='nav-link'> Contatos </a> </li>
+            </ul>
         </header>
     );
 }
