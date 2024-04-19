e75e82312baab6115b27316e8df8b43c4709f2db
Natan Cruz Oliveira <136939180+NatanCruzOliveira@users.noreply.github.com>
2024-04-19 01:47:56 -0300

    fvmrke

--- FRONT_PUSH/REACT.JS/blog/src/App.js
+++ FRONT_PUSH/REACT.JS/blog/src/App.js
@@ -1,12 +1,41 @@
 import Header from './components/Header';
 import Article from './components/Article';
 
+import {useState} from 'react'
+
 
 function App() {
+
+  let [carrinho, setcarrinho] = useState(0);
+
+  function additem(){
+    setcarrinho(carrinho +1);
+    console.log(carrinho);
+  }
+
+  function removeitem(){
+
+    if(carrinho > 0) {
+      setcarrinho(carrinho -1);
+    }
+    
+    console.log(carrinho);
+  }
+
   return (
     <>
       <Header></Header>
       <Article></Article>
+
+      <h3>
+        Adicione a quantidade no carrinho: {carrinho}
+      </h3>
+
+    <div className='flex'>
+      <button className='additens' onClick = {additem}>+</button>
+      <button className ='removeitens' onClick = {removeitem}>-</button>
+    </div>
+
     </>
     
   );
