d407671a0c9d332cc8b588d0280c278fd1eb5097
Natan Cruz Oliveira <136939180+NatanCruzOliveira@users.noreply.github.com>
2024-04-28 16:43:15 -0300

    zx s

--- FRONT_PUSH/REACT.JS/blog/src/pages/Main/index.js
+++ FRONT_PUSH/REACT.JS/blog/src/pages/Main/index.js
@@ -1,5 +1,5 @@
-import api from '../../services/api'
-import post from '../../components/Post'
+import api from '../../services/api';
+import post from '../../components/Post';
 
 
 function Main() {
